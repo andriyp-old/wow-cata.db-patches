4c20dc8a84501db8effd3628e6eb82ebd4550a56
quuxity <quuxity@gmail.com>
2011-09-07 12:20:00 +0300

    Reworked speech: now uses `creature_text` table which is collision-free, but has no built-in localization support (so i'll start to fork it into separate localized-scripts).

--- the-bastion-of-twilight.sql
+++ the-bastion-of-twilight.sql
@@ -24,55 +24,40 @@ INSERT INTO `creature_model_info` VALUES
     (34335, 0, 40, 2, 0);
 
 -- speech
-DELETE FROM `script_texts` WHERE `npc_entry` = 45213;
-INSERT INTO `script_texts`
-    (`npc_entry`,`entry`,`sound`,`content_default`,`content_loc8`) 
+DELETE FROM `creature_text` WHERE `entry` = 45213;
+INSERT INTO `creature_text`
+    (`entry`,`id`,`probability`,`sound`,`text`)
 VALUES
-    (45213, -452130, 20199 -- aggro
-    , 'We were fools to entrust an imbecile like Cho''gall with such a sacred duty. I will deal with you intruders myself!'
-    , 'Нельзя было доверять этому идиоту Чо''галлу такое важное поручение. Я сама с вами разберусь!'),
-    (45213, -452131, 20200 -- death
-    , 'Deathwing... I have fallen... the brood... is lost...'
-    , 'Смертокрыл... я подвела тебя... наш род... обречён...'),
-    (45213, -452132, 20201 -- kill-a
-    , 'My brood will feed on your bones!'
-    , 'Мои дети обглодают твои кости!'),
-    (45213, -452133, 20202 -- kill-b
-    , 'Powerless...'
-    , 'Слабое существо'),
-    (45213, -452134, 20203 -- losing
-    , 'You mistake this for weakness? FOOL!'
-    , 'Ты так в этом уверен? Глупец!'),
-    (45213, -452135, 20204 -- phase-a
-    , 'I tire of this! Do you see this clutch amidst which you stand? I have nurtured the spark within them, but that lifeforce is, and always will be, MINE! Behold... power beyond your comprehension!'
-    , 'Вы меня утомляете! Искры силы, вложенные в каждого из моих детей, принадлежат мне. Узрите же мощь, которую вы и представить себе не могли!'),
-    (45213, -452136, 20205 -- phase-b
-    , 'This will be your tomb as well as theirs!'
-    , 'Вы умрёте так же, как они!'),
-    (45213, -452137, 20206 -- phase-c
-    , 'Enough! Drawing upon this source will set us back months... you should feel honored to be worthy of this expenditure! NOW DIE!'
-    , 'Довольно! Это может затянуться на месяцы... я окажу тебе честь и потрачу на тебя весь запас энергии! ПОРА УМЕРЕТЬ!'),
-    (45213, -452138, 20207 -- spec-a
-    , 'Feed, children! Take your fill from their meaty husks!'
-    , 'Ешьте, дети мои! Пусть их мясо насытит вас!'),
-    (45213, -452139, 20208 -- spec-b
-    , 'The energy infuse within my clutch is mine to reclaim!'
-    , 'Я наделила своих детей силой и я же заберу её обратно!'),
-    (45213, -4521310, 20209 -- spec-c
-    , 'SUFFER!'
-    , 'СТРАДАНИЯ!'),
-    (45213, -4521311, 20210 -- spec-d
-    , 'FEEL MY HATRED!'
-    , 'ИСПЕПЕЛЯЮЩАЯ НЕНАВИСТЬ!'),
-    (45213, -4521312, 20211 -- spec-e
-    , 'Come forth children of Twilight!'
-    , 'Придите, исчадия сумрака!'),
-    (45213, -4521313, 20212 -- spec-f
-    , 'YOUR MOTHER CALLS!'
-    , 'ВАША МАТЬ ЗОВЁТ!'),
-    (45213, -4521314, 20213 -- winning
-    , 'My brood will feast upon your essence!'
-    , 'Мои дети полакомятся твоей плотью!');
+    (45213, 0, 0, 20199 -- aggro
+    , 'We were fools to entrust an imbecile like Cho''gall with such a sacred duty. I will deal with you intruders myself!'),
+    (45213, 1, 0, 20200 -- death
+    , 'Deathwing... I have fallen... the brood... is lost...'),
+    (45213, 2, 0, 20201 -- kill-a
+    , 'My brood will feed on your bones!'),
+    (45213, 3, 0, 20202 -- kill-b
+    , 'Powerless...'),
+    (45213, 4, 0, 20203 -- losing
+    , 'You mistake this for weakness? FOOL!'),
+    (45213, 5, 0, 20204 -- phase-a
+    , 'I tire of this! Do you see this clutch amidst which you stand? I have nurtured the spark within them, but that lifeforce is, and always will be, MINE! Behold... power beyond your comprehension!'),
+    (45213, 6, 0, 20205 -- phase-b
+    , 'This will be your tomb as well as theirs!'),
+    (45213, 7, 0, 20206 -- phase-c
+    , 'Enough! Drawing upon this source will set us back months... you should feel honored to be worthy of this expenditure! NOW DIE!'),
+    (45213, 8, 0, 20207 -- spec-a
+    , 'Feed, children! Take your fill from their meaty husks!'),
+    (45213, 9, 0, 20208 -- spec-b
+    , 'The energy infuse within my clutch is mine to reclaim!'),
+    (45213, 10, 0, 20209 -- spec-c
+    , 'SUFFER!'),
+    (45213, 11, 0, 20210 -- spec-d
+    , 'FEEL MY HATRED!'),
+    (45213, 12, 0, 20211 -- spec-e
+    , 'Come forth children of Twilight!'),
+    (45213, 13, 0, 20212 -- spec-f
+    , 'YOUR MOTHER CALLS!'),
+    (45213, 14, 0, 20213 -- winning
+    , 'My brood will feast upon your essence!');
 
 -- Pulsing Twilight Egg :
 DELETE FROM `creature_template` WHERE `entry` = 46842;
